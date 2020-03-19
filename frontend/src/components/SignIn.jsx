4c5e2c140baaceb9dcc38518c451c1a6fc9c1170
남우석 <hinyong22@gmail.com>
2020-03-20 01:14:18 +0900

    로그인 회원가입페이지 생성 및 router 구현

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -4,7 +4,11 @@ export default class SignIn extends React.Component {
   render() {
     return (
       <div>
-        SignIn
+        <form>
+        <input type="email" placeholder="Your Email"/><br/>
+          <input type="password" placeholder="Your Password"/><br/>
+          <input type="submit" value="Sign In"/> 
+        </form>
       </div>
     )
   }
--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -4,7 +4,12 @@ export default class SignUp extends React.Component {
   render() {
     return(
       <div>
-        Sign Up
+        <form >
+          <input type="text" placeholder="Your Name"/><br/>
+          <input type="email" placeholder="Your Email"/><br/>
+          <input type="password" placeholder="Your Password"/><br/>
+          <input type="submit" value="Sign Up"/> 
+        </form>
       </div>
     )
   }
